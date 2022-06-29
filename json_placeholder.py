30b782b8ff9bcdfe469cbb53530d8b73c0e0c96f
FranciscoEP <francisco.eugp@gmail.com>
2022-06-28 22:38:48 -0500

    scheduled task added

--- json_placeholder.py
+++ json_placeholder.py
@@ -1,24 +1,39 @@
 import requests
+from datetime import timedelta
+import json
+
 from prefect import task, Flow
+from prefect.schedules import IntervalSchedule
 
-API = 'https://jsonplaceholder.typicode.com/posts'
+API = 'https://jsonplaceholder.typicode.com/posts/1'
+schedule = IntervalSchedule(interval=timedelta(minutes=1))
 
-
-@task
+@task(log_stdout=True, max_retries=3, retry_delay=timedelta(minutes=1))
 def extract(url):
+    print("Obtaining data from: ", url)
     response = requests.get(url)
-    response = response.json()
-    return response
+    print("Status code: ", response.status_code)
+    response = json.loads(response.text)
+    with open('./transformed_data.json', 'w', encoding='utf-8') as f:
+        json.dump(response, f, ensure_ascii=False, indent=4)
 
 
-@task
+@task(log_stdout=True)
+def transform(raw):
+    print("Info: transforming raw data")
+    return "title",raw['title']
+
+
+@task(log_stdout=True)
 def load(response):
-    print([data["title"] for data in response])
+    print("Load initialized")
+    print(response)
 
 
-with Flow("JSON Placeholder") as flow:
+with Flow("JSON Placeholder", schedule) as flow:
     raw = extract(API)
-    load(raw)
+    transformed_data = transform(raw)
+    load(transformed_data)
 
 if __name__ == "__main__":
     flow.run()
